acb1952e1fea9b0cc4c13cb578c6edcafb768aa4
guixiaole <616098292@qq.com>
2021-04-15 16:56:33 +0800

    2021/4/15 实现全程记录标记

--- src/main/java/com/gxl/trainerror/mapper/StepAnalysisMapper.java
+++ src/main/java/com/gxl/trainerror/mapper/StepAnalysisMapper.java
@@ -1,7 +1,12 @@
 package com.gxl.trainerror.mapper;
 
+import com.gxl.trainerror.bean.StepAnalysis;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface StepAnalysisMapper {
+    @Select("select * from step_analysis")
+    public StepAnalysis selectByFileId(Integer fileId);
+    public Integer updateAnyStep(StepAnalysis stepAnalysis);
 }
---
+++ src/main/java/com/gxl/trainerror/service/StepInfoService.java
@@ -0,0 +1,7 @@
+package com.gxl.trainerror.service;
+
+import com.gxl.trainerror.bean.StepInfo;
+
+public interface StepInfoService {
+    public Integer insertStartEnd(StepInfo stepInfo);
+}
