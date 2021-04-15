45178dc72baa24f62e92fe3a6e874a5faa2fc873
guixiaole <616098292@qq.com>
2021-04-15 10:38:57 +0800

    五步闸点击

---
+++ src/main/java/com/gxl/trainerror/bean/StepInfo.java
@@ -0,0 +1,21 @@
+package com.gxl.trainerror.bean;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("step_info")
+public class StepInfo {
+    /*
+     为五步闸具体的信息。
+     分别为起始，结束，以及项点的id；
+     */
+    private Integer id;
+    private Integer startXiangDian;
+    private Integer endXiangDian;
+    private Integer xiangDianID;
+}
