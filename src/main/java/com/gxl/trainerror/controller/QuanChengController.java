2d98bf0f7a7f684b2b4d48a8d338f139d7206fa6
guixiaole <616098292@qq.com>
2021-04-20 11:01:33 +0800

    2021/4/20 完善

--- src/main/java/com/gxl/trainerror/controller/QuanChengController.java
+++ src/main/java/com/gxl/trainerror/controller/QuanChengController.java
@@ -2,8 +2,11 @@ package com.gxl.trainerror.controller;
 
 import com.gxl.trainerror.bean.FileInfo;
 import com.gxl.trainerror.bean.QuanCheng;
+import com.gxl.trainerror.bean.StepAnalysis;
 import com.gxl.trainerror.service.FileInfoService;
 import com.gxl.trainerror.service.QuanChengService;
+import com.gxl.trainerror.service.StepAnalysisService;
+import com.gxl.trainerror.service.StepInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +22,17 @@ public class QuanChengController {
     private QuanChengService quanChengService;
     @Autowired
     private FileInfoService fileInfoService;
+    @Autowired
+    private StepAnalysisService stepAnalysisService;
+    @Autowired
+    private StepInfoService stepInfoService;
     @RequestMapping("/quancheng")
     public String getQuanCheng(Integer id, Model model){
         List<QuanCheng> quanChengs = quanChengService.selectByFileAscXuhao(id);
 //        System.out.println(quanChengs.size());
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+        StepAnalysis stepAnalysis = stepAnalysisService.selectByFileID(id);
+        model.addAttribute("stepAnalysis",stepAnalysis);
         model.addAttribute("quanChengs",quanChengs);
         if (quanChengs.size()>0){
             Integer fileid = quanChengs.get(0).getFileId();
