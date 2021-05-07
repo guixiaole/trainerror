201d5374a137d544107ba168002554bb1cc9aec4
guixiaole <616098292@qq.com>
2021-05-07 23:11:15 +0800

    2021/4/30 展示版本结束

---
+++ src/main/java/com/gxl/trainerror/bean/FiveStepTemplate.java
@@ -0,0 +1,27 @@
+package com.gxl.trainerror.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+@NoArgsConstructor
+@AllArgsConstructor
+public class FiveStepTemplate {
+    /*
+    意为五步闸分析的模板化
+     */
+    //意为这一块的起始和结束。
+    private Integer start;
+    private Integer end;
+    //意为上一模板和下一模板是高还是低，相对于自身来说。
+    private Date startTime;
+    private Date endTime;
+    // 其中1为高，-1为低
+    private Integer lastEnd;
+    private Integer nextStart;
+    //0为这段稳定，1为这段上升，-1为这段下降。
+    private Integer isStable;
+    //使用链表进行构成
+    private FiveStepTemplate next;
+}
---
+++ src/main/java/com/gxl/trainerror/util/StepTemplateUtil.java
@@ -0,0 +1,23 @@
+package com.gxl.trainerror.util;
+
+import com.gxl.trainerror.bean.FileInfo;
+import com.gxl.trainerror.bean.FiveStepTemplate;
+import com.gxl.trainerror.bean.QuanCheng;
+
+import java.util.List;
+
+public class StepTemplateUtil {
+    /*
+        五步闸分析模板化
+     */
+    public static List<FiveStepTemplate> FiveStepTemplateAnalysis(List<FileInfo> quanChengs){
+        //进行五步闸模板化具体方法,传入的数据是全程记录
+        //将传入的时间进行模板化
+
+        for (int i = 1;i<quanChengs.size();i++){
+            //首先获取的是时间
+
+        }
+        return null;
+    }
+}
